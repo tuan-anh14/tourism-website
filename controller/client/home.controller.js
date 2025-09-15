f978b685fec5f023688e87b9855e225ce608ea31
tuan-anh14 <thieutuananh14092k4@gmail.com>
2025-09-15 22:44:25 +0700

    Add pages

--- controller/client/home.controller.js
+++ controller/client/home.controller.js
@@ -9,3 +9,34 @@ module.exports.events = (req, res) => {
         pageTitle: "Events"
     })
 }
+
+// Client content pages
+module.exports.attractions = (req, res) => {
+    res.render("client/pages/attraction.ejs", {
+        pageTitle: "Điểm tham quan"
+    })
+}
+
+module.exports.accommodation = (req, res) => {
+    res.render("client/pages/accommodation.ejs", {
+        pageTitle: "Lưu trú"
+    })
+}
+
+module.exports.cuisine = (req, res) => {
+    res.render("client/pages/cuisine.ejs", {
+        pageTitle: "Ẩm thực"
+    })
+}
+
+module.exports.transportation = (req, res) => {
+    res.render("client/pages/transportation.ejs", {
+        pageTitle: "Di chuyển"
+    })
+}
+
+module.exports.entertainment = (req, res) => {
+    res.render("client/pages/entertainment.ejs", {
+        pageTitle: "Giải trí"
+    })
+}
--- routes/client/home.route.js
+++ routes/client/home.route.js
@@ -5,5 +5,10 @@ const homeController = require("../../controller/client/home.controller")
 
 route.get('/', homeController.index)
 route.get('/events', homeController.events)
+route.get('/attractions', homeController.attractions)
+route.get('/accommodation', homeController.accommodation)
+route.get('/cuisine', homeController.cuisine)
+route.get('/transportation', homeController.transportation)
+route.get('/entertainment', homeController.entertainment)
 
 module.exports = route; 
