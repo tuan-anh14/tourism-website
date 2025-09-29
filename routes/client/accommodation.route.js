84c3e9d7be8f84dcb421ba058e5dbadcb94b8d87
tuan-anh14 <thieutuananh14092k4@gmail.com>
2025-09-29 14:47:23 +0700

    Detail accommondation

--- routes/client/accommodation.route.js
+++ routes/client/accommodation.route.js
@@ -4,4 +4,5 @@ const route = express.Router()
 const accommodationController = require("../../controller/client/accommodation.controller") 
 
 route.get('/', accommodationController.accommodation)
+route.get('/:slug', accommodationController.accommodationDetail)
 module.exports = route; 
