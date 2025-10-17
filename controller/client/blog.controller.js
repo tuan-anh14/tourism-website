a103cc4ada5470b7571d11925f27ecd95f97c9c8
tuan-anh14 <thieutuananh14092k4@gmail.com>
2025-10-17 14:55:27 +0700

    Add detail page for Old Quarter blog; implement route and update blog overview with link to new page

--- controller/client/blog.controller.js
+++ controller/client/blog.controller.js
@@ -4,3 +4,19 @@ module.exports.index = (req, res) => {
     page: "blog",
   });
 };
+
+module.exports.detailOldQuarter = (req, res) => {
+  res.render("client/pages/blog/detail-old-quarter", {
+    pageTitle: "Khám phá phố cổ Hà Nội - Hành trình 36 phố phường",
+    page: "blog",
+    meta: {
+      description:
+        "Dạo bước qua những con phố cổ kính, khám phá văn hóa và ẩm thực đặc sắc của Hà Nội trong hành trình 36 phố phường.",
+      keywords:
+        "phố cổ Hà Nội, 36 phố phường, du lịch Hà Nội, văn hóa, ẩm thực",
+      image: "/client/img/pho-co.jpg",
+      author: "Admin",
+      date: "2024-01-15",
+    },
+  });
+};
--- routes/client/blog.route.js
+++ routes/client/blog.route.js
@@ -5,4 +5,7 @@ const blogController = require("../../controller/client/blog.controller");
 // Blog page route
 router.get("/", blogController.index);
 
+// Blog detail: Old Quarter - 36 streets
+router.get("/pho-co-ha-noi-36-pho-phuong", blogController.detailOldQuarter);
+
 module.exports = router;
