5adf93e43191ddd66695ca50eb5bdf0cca2c8098
tuan-anh14 <thieutuananh14092k4@gmail.com>
2025-10-22 02:47:59 +0700

    Update

--- routes/client/blog.route.js
+++ routes/client/blog.route.js
@@ -9,5 +9,13 @@ router.get("/", blogController.index);
 router.get("/bai-1", blogController.articleOne);
 router.get("/bai-2", blogController.articleTwo);
 router.get("/bai-3", blogController.articleThree);
+router.get("/bai-4", blogController.articleFour);
+router.get("/bai-5", blogController.articleFive);
+router.get("/bai-6", blogController.articleSix);
+router.get("/bai-7", blogController.articleSeven);
+router.get("/bai-8", blogController.articleEight);
+router.get("/bai-9", blogController.articleNine);
+router.get("/bai-10", blogController.articleTen);
+router.get("/bai-11", blogController.articleEleven);
 
 module.exports = router;
